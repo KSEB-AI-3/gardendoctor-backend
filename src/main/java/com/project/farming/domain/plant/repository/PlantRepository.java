c4b779f4b13fd74ea4dcf2622ff3439d080b96b5
kih056 <207148057+kih056@users.noreply.github.com>
2025-07-07 10:58:25 +0900

    Plant 기능 추가

--- src/main/java/com/project/farming/domain/plant/repository/PlantRepository.java
+++ src/main/java/com/project/farming/domain/plant/repository/PlantRepository.java
@@ -4,6 +4,10 @@ import com.project.farming.domain.plant.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+    boolean existsByName(String name);
+    List<Plant> findAllByOrderByName();
+    Optional<Plant> findByName(String name);
 }
